89f5008bd9ca9f7bc5243f35a0cbfed1759cfa05
CamaroSS844 <t.fsialumba@gmail.com>
2023-09-27 06:09:39 +0200

    chat screen

--- App.js
+++ App.js
@@ -2,6 +2,26 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import Chat from "./screens/Chat";
+
+const Stack = createStackNavigator();
+
+function ChatStack(){
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  )
+}
+
+function RootNAvigator(){
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  )
+}
+
 export default function App() {
   return (
     <View >
